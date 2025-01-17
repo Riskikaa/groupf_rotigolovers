6e034d8adf3f5842883dff410a8673c165e5e55c
ester-04-jk <esterjunitarsamofo@gmail.com>
2025-01-18 06:53:57 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:groupf_rotigolovers/firebase_options.dart';
 
-// Pages
-// import 'package:rotigolovers_groupf/pages/welcome_page.dart';
-// import 'package:rotigolovers_groupf/pages/login_page.dart';
-// import 'package:rotigolovers_groupf/pages/register_page.dart';
-// import 'package:rotigolovers_groupf/pages/login_admin.dart';
-// import 'package:rotigolovers_groupf/pages/halaman_menu.dart';
-// import 'package:rotigolovers_groupf/pages/halaman_laporan_penjualan.dart';
-// import 'package:rotigolovers_groupf/pages/halaman_chef.dart';
-// import 'package:rotigolovers_groupf/pages/form_add_menu.dart';
-// import 'package:rotigolovers_groupf/pages/halaman_detail_penjualan.dart';
+import '../pages/welcome_page.dart';
+import '../pages/login_page.dart';
+import '../pages/register_page.dart';
+import '../pages/login_admin.dart';
+import '../pages/halaman_menu.dart';
+import '../pages/halaman_laporan_penjualan.dart';
+import '../pages/halaman_chef.dart';
+import '../pages/form_add_menu.dart';
+import '../pages/halaman_detail_penjualan.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,39 +29,39 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       initialRoute: 'welcome_page',
       onGenerateRoute: (settings) {
-        // switch (settings.name) { 
-        //   case 'welcome_page':
-        //     return MaterialPageRoute(builder: (_) => const WelcomePage());
-        //   case 'login_page':
-        //     return MaterialPageRoute(builder: (_) => LoginPage());
-        //   case 'register_page':
-        //     return MaterialPageRoute(builder: (_) => RegisterPage());
-        //   case 'login_admin':
-        //     return MaterialPageRoute(builder: (_) => AdminLoginPage());
-        //   case 'halaman_menu':
-        //     return MaterialPageRoute(builder: (_) => const RotigoloversList());
-        //   case 'halaman_laporan_penjualan':
-        //     return MaterialPageRoute(
-        //         builder: (_) => const RotigoloversLaporan());
-        //   case 'halaman_chef':
-        //     return MaterialPageRoute(builder: (_) => const RotigoloversChef());
-        //   case 'form_add_menu':
-        //     return MaterialPageRoute(builder: (_) => const MenuFormAdd());
-        //   case 'halaman_detail_penjualan':
-        //     if (settings.arguments is Map) {
-        //       final args = settings.arguments as Map;
-        //       return MaterialPageRoute(
-        //         builder: (_) => RotigoloversDetail(
-        //           tanggal: args['tanggal'],
-        //           laporan: args['laporan'],
-        //         ),
-        //       );
-        //     }
-        //     return _errorRoute();
-        //   default:
-        //     return _errorRoute();
+        switch (settings.name) {
+          case 'welcome_page':
+            return MaterialPageRoute(builder: (_) => const WelcomePage());
+          case 'login_page':
+            return MaterialPageRoute(builder: (_) => LoginPage());
+          case 'register_page':
+            return MaterialPageRoute(builder: (_) => RegisterPage());
+          case 'login_admin':
+            return MaterialPageRoute(builder: (_) => AdminLoginPage());
+          case 'halaman_menu':
+            return MaterialPageRoute(builder: (_) => const RotigoloversList());
+          case 'halaman_laporan_penjualan':
+            return MaterialPageRoute(
+                builder: (_) => const RotigoloversLaporan());
+          case 'halaman_chef':
+            return MaterialPageRoute(builder: (_) => const RotigoloversChef());
+          case 'form_add_menu':
+            return MaterialPageRoute(builder: (_) => const MenuFormAdd());
+          case 'halaman_detail_penjualan':
+            if (settings.arguments is Map) {
+              final args = settings.arguments as Map;
+              return MaterialPageRoute(
+                builder: (_) => RotigoloversDetail(
+                  tanggal: args['tanggal'],
+                  laporan: args['laporan'],
+                ),
+              );
+            }
+            return _errorRoute();
+          default:
+            return _errorRoute();
         }
-      //},
+      },
     );
   }
 
